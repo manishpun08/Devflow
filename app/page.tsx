4e89c091a41646a2825dae590cf6e801496b9858
manishpun08 <manishpunmagar6@gmail.com>
2025-01-23 18:37:57 +0545

    eslint and prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Home = () => (
-  <h1 className="font-3xl text-violet-700">Welcome to the world</h1>
+  <h1 className="text-3xl font-black text-violet-700">
+    Welcome to the world of nextjs
+  </h1>
 );
 
 export default Home;

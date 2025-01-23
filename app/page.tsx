a818d9e43236071b62e1e1a2020dfbad77f94ff4
manishpun08 <manishpunmagar6@gmail.com>
2025-01-23 19:49:08 +0545

    Setup for Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Home = () => <h1 className="h1-bold ">Welcome to the world of nextjs</h1>;
+const Home = () => {
+  return (
+    <>
+      <h1 className="h1-bold ">Welcome to the world of nextjs</h1>
+      <h1 className="h1-bold font-inter">Welcome to the world of nextjs</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of nextjs
+      </h1>
+    </>
+  );
+};
 
 export default Home;

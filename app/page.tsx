665fb2a08413a8abf4001c46a62f398f2bd87e9d
manishpun08 <manishpunmagar6@gmail.com>
2025-01-23 20:13:12 +0545

    Assets and MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold ">Welcome to the world of nextjs</h1>
+      <h1 className="h1-bold text-primary-500 font-black">
+        Welcome to the world of nextjs
+      </h1>
       <h1 className="h1-bold font-inter">Welcome to the world of nextjs</h1>
       <h1 className="h1-bold font-space-grotesk">
         Welcome to the world of nextjs

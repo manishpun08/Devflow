630c6943ebc8145174689f2d49576339863b6158
manishpun08 <manishpunmagar6@gmail.com>
2025-01-24 14:06:21 +0545

    Installation of next-theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold text-primary-500 font-black">
-        Welcome to the world of nextjs
-      </h1>
-      <h1 className="h1-bold font-inter">Welcome to the world of nextjs</h1>
-      <h1 className="h1-bold font-space-grotesk">
+      <h1 className="h1-bold font-black text-primary-500">
         Welcome to the world of nextjs
       </h1>
     </>

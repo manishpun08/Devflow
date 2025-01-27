131b88d2ac8ddc46e5027e067b7112578337de1a
manishpun08 <manishpunmagar6@gmail.com>
2025-01-27 14:34:24 +0545

    Implement Right Sidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { techMap } from "@/constants/tecMap";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getDevIconClassName = (techName: string) => {
+  const normalizeTechName = techName.replace(/[ .]/g, "").toLowerCase();
+
+  return techMap[normalizeTechName]
+    ? `${techMap[normalizeTechName]} colored`
+    : "devicon-react-plain";
+};

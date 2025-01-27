77973e09d952cf65d5c4a09e0c26a3b630d9e448
manishpun08 <manishpunmagar6@gmail.com>
2025-01-27 18:48:13 +0545

    Home Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
+
 const Home = async () => {
   return (
     <>
-      <h1 className="h1-bold font-black text-primary-500">
-        Welcome to the world of nextjs
-      </h1>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center ">
+        <h1 className="h1-bold text-dark100_light900 ">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };

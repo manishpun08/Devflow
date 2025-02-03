0a86349a9737cf1494370c34d519e6c7617f92cc
manishpun08 <manishpunmagar6@gmail.com>
2025-02-03 13:19:59 +0545

    Create Tag Models

--- database/user.model.ts
+++ database/user.model.ts
@@ -26,6 +26,6 @@ const UserSchema = new Schema<IUser>(
   }
 );
 
-const User = models?.user || model<IUser>("User", UserSchema);
+const User = models?.User || model<IUser>("User", UserSchema);
 
 export default User;
